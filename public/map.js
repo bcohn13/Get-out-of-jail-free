8f231596248875d741b92f4019c8130c97a782f0
charlie <70538109+cbelinky@users.noreply.github.com>
2020-12-10 15:11:51 -0500

    Update map.js
    changed api to moco dataset

--- public/map.js
+++ public/map.js
@@ -25,7 +25,7 @@ let crimeData=[];
 
   Promise.all([
     fetch(
-      "https://data.princegeorgescountymd.gov/resource/wb4e-w4nf.json"
+      "https://data.montgomerycountymd.gov/resource/icn6-v9z3.json"
     )
   ]).then(async ([response]) => {
     const responseData = await response.json();
