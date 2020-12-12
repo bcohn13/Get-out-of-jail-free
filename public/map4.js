67d08b366cc3b5f50893b7cb60cb61f11257fc91
alexeyjeffers <70725010+alexeyjeffers@users.noreply.github.com>
2020-12-11 21:33:29 -0500

    Update map4.js

--- public/map4.js
+++ public/map4.js
@@ -40,14 +40,8 @@ var map = L.map("map", {preferCanvas: true}).setView([39.1433333,-77.2016667], 1
 }
 
     for (const key in zipCodes) {
-    
-   
-        
-        
             layerGroup.addLayer(
-            
             L.marker([zipCodes[key].lat,zipCodes[key].lng], {icon}).bindPopup(
-      
             )
           );
         }
